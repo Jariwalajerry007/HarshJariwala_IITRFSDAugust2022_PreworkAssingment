036daebdf620bb2b7bda36002cca1d5e47deab9b
Harshkumar Anilbhai Jariwala <109514142+Jariwalajerry007@users.noreply.github.com>
2022-08-21 18:15:17 +0530

    BASIC TO ADVANCED
    I want to rerun the whole loop in case any number/function or both is not as complier wish.
    Also, I want to make it look easy to run code again just by asking the input.
    So, here are some additions like equals(); with String and bound in While loop, which will help calling the Switch Case loop and Number loop again and Again Until tester enters other then Y or YES.

--- Switchdowhile.java
+++ Switchdowhile.java
@@ -4,41 +4,49 @@ import java.util.Scanner;
 
 public class Switchdowhile {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Reply="YES";// TO RUN WHOLE LOOP AGAIN
+        while(Reply.equals("YES"))
+        {
+		System.out.println("Enter the Number"+'\n');
 		Scanner myObj=new Scanner(System.in);
-        // To Check and Print, Enter the Number 
-        System.out.println("Enter the Number"+'\n');
         int inPut = myObj.nextInt();
-       
+        int I;
+         
+         String reply="Y";// TO RUN ONLY SWITCH CASE AGAIN
+         while(reply.equals("Y"))
+         {
+
         System.out.println("We will check Number "+inPut+" for diffrent cases"+'\n');
         System.out.println("Please entre the case no. to display their possible result"+'\n');
         System.out.println("Note: There are 4 cases to check, please enter accordingly"+'\n'+"(i.e)"+'\n'+ "1:Check for palindrome number"+'\n'+ "2:Prints the pattern of stars in decreasing order based on the input"+'\n'+"3:Check whether the input number is a prime number"+'\n'+"4:Print Fibonacci series of size as entered with first two numbers as 0, 1");
-        
-        int I;
+        System.out.println('\n'+"Note: Whenever ask for (Y/N or YES/NO), you have to type case sensitively Y or YES accordingly "); 
         Scanner console = new Scanner(System.in);
         I = console.nextInt();
         switch(I){
             case 1:
-                System.out.println("We will check weather "+inPut+" is Palindrome Number or not...."+'\n');
+            {
+            	System.out.println("We will check weather "+inPut+" is Palindrome Number or not...."+'\n');
                int z,y=0,x;
                x = inPut;
                do{
                z = x % 10; 
                y=(y*10)+z;
                x=x/10;
-                 }
-                         while(x>0);
+                 }while(x>0);
                          // if given number equal to sum than number is palindrome otherwise not palindrome
               	        	if(y==inPut)    
   	                     	{
-               		     	  System.out.println(inPut+" is Palindrome Number "+'\n'); 
+               		     	  System.out.println(inPut+" is Palindrome Number "+'\n'+"Do you want to continue checking results with the different cases(Y/N)"); 
                	            	}
-                   	         	else    
-                  		    	System.out.println(inPut+" is not Palindrome Number"+'\n');
-                                           break ;
-                         
+                   	         	else {   
+                  		    	System.out.println(inPut+" is not Palindrome Number"+'\n'+"Do you want to continue checking results with the different cases(Y/N)");
+                   	         	}  break ;
+            }
         case 2:
-              int row=1,column=inPut;
+        {
+        	int row=1,column=inPut;
               int k;
               do{
                  k=column;
@@ -54,10 +62,11 @@ public class Switchdowhile {
                              System.out.println(" ");
                              row++;
                             }while (row<=inPut);
-            System.out.println("case 2 is completed");
+            System.out.println("Do you want to continue checking results with the different cases(Y/N)");
              break;
+        }
         case 3:
-         
+        {
             int q,r=0;
              q=1;
             do{
@@ -68,47 +77,45 @@ public class Switchdowhile {
                q++;
               }while(q<=inPut);
            if(r==2)
-          System.out.println(inPut+"is Prime Number");
+          System.out.println(inPut+" is Prime Number"+'\n'+"Do you want to continue checking results with the different cases(Y/N)");
           else
-          System.out.println(inPut+"is not Prime Number");
+          System.out.println(inPut+" is not Prime Number"+'\n'+"Do you want to continue checking results with the different cases(Y/N)");
              break;
+        }
         case 4:
+        {
             System.out.println("Fibonacci number of "+inPut+'\n');
              int m1=0,m2=1,m3,n;
                  System.out.print(m1+" "+m2+" ");
                   n=2;//array count
                    do
-                     {
+                     {               	   
                        m3=m1+m2;
-                        System.out.print(" "+m3+" ");   
+                        System.out.print(" "+m3+" ");                          
                        ++n;
                        m1=m2;
-                        m2=m3;
+                        m2=m3;                        
                         }while(n<inPut);
                          System.out.print("");
+                         System.out.println('\n'+"Do you want to continue checking results with the different cases(Y/N)");
                          break;
-                      default:System.out.println("Invalid case number!"); 
-                      console.close();     
-  }
- while(true){
-     System.out.println("To continue with diffrent case if you want to contine,  Select: (Y/N) ");
-  Scanner scanAgain = new Scanner(System.in);
-          String input = scanAgain.nextLine();
-          if (input.equals("Y"))
-        	  
-          {
-          System.out.println("You have to select any case again");
-          System.out.println("Please enter any Case number :");
-          break;
-            } else if(input.equals("N"))
-            	scanAgain.close();	
-            {
-            System.out.println("Have a nice time"); 
-            
-            break;            
-            }
-    }
- myObj.close();
+        }
+             
+          default:System.out.println("Invalid case number!"+'\n'); 
+          System.out.println("Do you want to continue with the ABOVE cases (Y/N) if Y then please select from 1/2/3/4 only");             
+
+}// End of Switch
+        
+        reply = console.next();
+        
+}//case while end       
+       System.out.println("Out of Cases");
+       System.out.println("TYPE (YES) TO CONTINUE WITH NEW NUMBER & (NO) TO END");
+       Reply = myObj.next();
+       
+	}//Number loop	
+	System.out.println("THE END");
+
   }
   }
 		
